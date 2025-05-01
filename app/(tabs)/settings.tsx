04faaf53d9426193de2071e126418e36e8f86324
Naman Doshi <64643300+namandoshi8@users.noreply.github.com>
2025-05-02 01:01:30 +0530

    seetings scrren touch up

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,22 +1,101 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Switch, Text, TouchableOpacity, View } from "react-native";
+import React, { useState } from "react";
+import { Stack } from "expo-router";
+import { Colors } from "@/constants/Colors";
+import { MaterialIcons } from "@expo/vector-icons";
 
-type Props = {}
+type Props = {};
 
 const Page = (props: Props) => {
+  const [enabled, setEnabled] = useState(false);
+  const toggleSwitch = () => setEnabled((previousState) => !previousState);
   return (
-    <View style={styles.container}>
-      <Text>Settings Screen</Text>
-    </View>
-  )
-}
+    <>
+      <Stack.Screen
+        options={{
+          headerShown: true,
+        }}
+      />
+      <View style={styles.container}>
+        <TouchableOpacity style={styles.itemBtn}>
+          <Text style={styles.itemBtnText}>About</Text>
+          <MaterialIcons
+            name="keyboard-arrow-right"
+            size={24}
+            color={Colors.black}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.itemBtn}>
+          <Text style={styles.itemBtnText}>Send Feedback</Text>
+          <MaterialIcons
+            name="keyboard-arrow-right"
+            size={24}
+            color={Colors.black}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.itemBtn}>
+          <Text style={styles.itemBtnText}>Privacy Policies</Text>
+          <MaterialIcons
+            name="keyboard-arrow-right"
+            size={24}
+            color={Colors.black}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.itemBtn}>
+          <Text style={styles.itemBtnText}>Terms of Use</Text>
+          <MaterialIcons
+            name="keyboard-arrow-right"
+            size={24}
+            color={Colors.black}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.itemBtn} onPress={toggleSwitch}>
+          <Text style={styles.itemBtnText}>Dark Mode</Text>
+          <Switch
+            trackColor={{ false: Colors.darkGrey, true: Colors.tint }}
+            thumbColor={Colors.white}
+            ios_backgroundColor={Colors.darkGrey}
+            onValueChange={toggleSwitch}
+            value={enabled}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.itemBtn}
+          onPress={() => {
+            // Handle logout logic here
+            console.log("Logout pressed");
+            alert("Logout pressed");
+          }}
+        >
+          <Text style={{ ...styles.itemBtnText, color: "red" }}>Logout</Text>
+          <MaterialIcons name="logout" size={24} color="red" />
+        </TouchableOpacity>
+      </View>
+    </>
+  );
+};
 
-export default Page
+export default Page;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    padding: 20,
   },
-})
+  itemBtn: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    backgroundColor: Colors.white,
+    paddingHorizontal: 16,
+    paddingVertical: 20,
+    borderRadius: 10,
+    borderBottomColor: Colors.background,
+    borderBottomWidth: 1,
+    marginBottom: 10,
+  },
+  itemBtnText: {
+    fontSize: 18,
+    fontWeight: "500",
+    color: Colors.black,
+  },
+});
